87c75500b3660001ec78087361d081d1ac0189a6
mhartlage3 <madeline.hartlage@gatech.edu>
2020-10-08 20:04:12 -0400

    planting and removing trees works

--- Arboratory/Farm.gd
+++ Arboratory/Farm.gd
@@ -1,29 +1,75 @@
 extends TileMap
 
+
+
 signal plot_clicked
+signal plant
+signal new_farm
+signal tree_removed
 
 var tile_size = get_cell_size()
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var farm_size = Vector2(10,10)
+var farm_size = Vector2(7,7)
 var farm = []
+var spot
+var remove_tree
+var planting_ready
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for x in range(farm_size.x):
 		farm.append([])
 		for y in range(farm_size.y):
-			farm[x].append(null)
-		
+			farm[x].append(false)
+	planting_ready = false
 
+#user is ready to plant a tree
+func _ready_to_plant():
+	planting_ready = true
+	
+#clears all farm plots
+func _clear_plots():
+	for x in range(farm_size.x):
+		for y in range(farm_size.y):
+			farm[x][y] = false
+	get_tree().call_group("trees","queue_free")
+	emit_signal("new_farm","New Farm")
+
+#clears single plot
+func _clear_single_plot():
+	remove_tree = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+#handles mouse-click
 func _input(event):
 	if Input.is_action_pressed("ui_click"):
-		#print("Mouse Click at: ",event.position)
-		var spot = "Tree planted in plot: " + str(int((event.position.x-80)/(64*.75))) + "," + str(int((event.position.y-40)/(64*.75)))
-		#print(int(event.position.x/64))
-		emit_signal("plot_clicked",spot)
+		
+		if event.position.x <896:
+			if planting_ready == true:
+				var plot_x = int((event.position.x)/(64*2))
+				var plot_y = int((event.position.y)/(64*2))
+				var tree_placement_x = ((plot_x+1)*64)+plot_x*64
+				var tree_placement_y = (plot_y-32)+plot_y*128
+			
+				if farm[plot_x][plot_y] == false:
+					#plant a new tree
+					spot = "Tree planted in plot: " + str(plot_x) + "," + str(plot_y)
+					farm[plot_x][plot_y] = true
+					emit_signal("plot_clicked",spot)
+					emit_signal("plant",tree_placement_x,tree_placement_y,plot_x,plot_y)
+			
+				elif remove_tree == true:
+					#remove single tree
+					farm[plot_x][plot_y] = false
+					emit_signal("plant",tree_placement_x,tree_placement_y,plot_x,plot_y)
+					emit_signal("tree_removed","Tree Removed")
+					remove_tree = false
+				
+				else:
+				#a tree is already in this plot
+					spot = "A tree is already here"
+					emit_signal("plot_clicked",spot)
+		
+		
