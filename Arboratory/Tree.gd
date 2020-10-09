bf5bb3a43bf11601e6c010fa627d12dae3a2ba65
Marin Hyatt <marin.hyatt@gmail.com>
2020-10-08 21:17:49 -0400

    Tried incorporating realtime halth deduction, failed

--- Arboratory/Tree.gd
+++ Arboratory/Tree.gd
@@ -15,34 +15,59 @@ signal deduct_health
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	"""
-	More temporary water functionality
-	"""
 	connect("deduct_health", self, "_on_deduct_health")
+	
+	
 
-"""
-Temporary water functionality
-"""
 
 func _process(delta):
-	#check to see whether enough time has passed in real life for plants to need water
+	#check to see whether enough time has passed in real life 
+	#for plants to need water
 	if (OS.get_unix_time() - last_watering_time >= TIME_UNTIL_DRY) :
 		#print("I need water!")
 		#decrease 1 point of health every 10 min\
 		#emit signal to start decrease of health
-		emit_signal("deduct_health")
+#		if $healthDeduction.time_left == 0:
+#			$healthDeduction.start()
+		var healthDeduction = 1
+		if (OS.get_unix_time() - prev_recorded_time != 0):
+			healthDeduction = \
+			(OS.get_unix_time() - prev_recorded_time) / $healthDeduction.wait_time
+		deduct_health(healthDeduction)
+	#sets previous recorded time to current time
+	#when user closes game, prev_recorded_time will be time of closing game
+	prev_recorded_time = OS.get_unix_time()
 		
 """
 Starts the health deduction timer if it's not already started
 """
-func _on_deduct_health():
+#func _on_deduct_health():
+#	#subtracts 1 * number of times health was supposed to 
+#	#be subtracted while game was off
+#	var healthDeduction = 1
+#	if (OS.get_unix_time() - closing_time != 0):
+#		healthDeduction = \
+#		(OS.get_unix_time() - prev_recorded_time) / $healthDeduction.wait_time
+#	print(healthDeduction)
+#	health -= healthDeduction
+#	print(health)
+
+func deduct_health(healthDeduction):
 	if $healthDeduction.time_left == 0:
 		$healthDeduction.start()
+	
+#	health -= healthDeduction
+#	print(health)
 
 func _on_waterButton_pressed():
 	print("I was watered!")
 	last_watering_time = OS.get_unix_time()
 
+
 func _on_healthDeduction_timeout():
 	health -= 1
-	print(health)
+	print("I need water!")
+	print("health: ", health)
+	
+
+	
