5dd25364c0c4725a2b3c706fdcf69a38d95502e0
StrawberryNougat <eleanormather02@gmail.com>
2020-11-11 03:23:10 -0500

    Changing file

--- Arboratory/MainMenu.gd
+++ Arboratory/MainMenu.gd
@@ -17,7 +17,7 @@ func _ready():
 
 
 func _on_TextureButton_pressed():
-	get_tree().change_scene("res://Farm.tscn") # Replace with function body.
+	get_tree().change_scene("res://Planting.tscn") # Replace with function body.
 
 
 func _on_VolumeButton_pressed():
