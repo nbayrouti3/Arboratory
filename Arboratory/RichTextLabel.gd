efb1fe8ce04d4fc3643d78892fe7059d9289cb34
nbayrouti3 <noura@beirutis.com>
2020-11-11 14:50:55 -0500

    Dialogue updates for level events

--- Arboratory/RichTextLabel.gd
+++ Arboratory/RichTextLabel.gd
@@ -8,10 +8,33 @@ var dialogue = [
 ]
 
 var dialogue_index = 0
+signal showSprite
+signal hideSprite
+var interactable = false
+
+func select_dialogue():
+	match Leveling.level:
+		1:
+			print('successfully did the thing')
+			dialogue = ["insert tutorial dialogue here",
+			"testing that it does the other thing"]
+			emit_signal("showSprite")
+		3:
+			dialogue = ["insert item dialogue here"]
+			emit_signal("showSprite")
+		5:
+			dialogue = ["insert dating tutorial dialogue here"]
+			emit_signal("showSprite")
+			interactable = true
+		_:
+			hide()
+			emit_signal("hideSprite")
 
 signal releaseTheButtons
 
+
 func _ready():
+	select_dialogue()
 	set_bbcode(dialogue[dialogue_index])
 	set_visible_characters(0)
 	set_process_input(true)
@@ -25,8 +48,11 @@ func _input(event):
 				dialogue_index += 1
 				set_bbcode(dialogue[dialogue_index])
 				set_visible_characters(0)
-			else:
+			elif interactable:
 				emit_signal("releaseTheButtons")
+			else:
+				emit_signal("hideSprite")
+				hide()
 		else: 
 			set_visible_characters(get_total_character_count())
 	
